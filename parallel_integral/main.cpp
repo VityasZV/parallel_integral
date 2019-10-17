819e9d61aa57dc809d1686231de53b406cd7e60c
Victor Zadyabin <viktor.zadyabin@mail.ru>
2019-10-17 18:46:33 +0300

    Initial Commit

---
+++ parallel_integral/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  parallel_integral
+//
+//  Created by Виктор Задябин on 17.10.2019.
+//  Copyright © 2019 Виктор Задябин. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
